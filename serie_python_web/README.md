0ec3dcf5432299f27dcd80f19774f21e622c0a5a
ANDERSON PEREIRA <andy2903.alp@gmail.com>
2024-03-24 21:14:25 -0300

    feat: adjust file docker-compose

--- serie_python_web/README.md
+++ serie_python_web/README.md
@@ -1,6 +1,7 @@
 # serie_python
  Postagem sobre conhecimentos gratuitos
-
+Antes execute o WireMock 
+ java -jar wiremock-standalone-3.4.2.jar --port 8081
 # Running docker
 docker build -t meu-fastapi-app .
 docker run -d -p 8000:8000 --name meu-fastapi-app-container meu-fastapi-app
--- serie_python_web/fast_api/main.py
+++ serie_python_web/fast_api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import requests
 import pybreaker
+import redis
 
 # Configuração do circuit breaker
 breaker = pybreaker.CircuitBreaker(
@@ -15,24 +16,39 @@ app = FastAPI()
 @breaker
 def call_external_service(url):
     response = requests.get(url)
-    return response.json()
+    return str(response.json())
 
+
+# Configuração do Redis para fallback
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+def buscar_dados(chave):
+    return redis_client.get(chave)
 
 # Rota do FastAPI para lidar com a chamada ao serviço externo
 @app.get("/pokemons")
 def handle_call_service():
     service_url = "http://localhost:8081/v1/pokemons"
+    chave = "minha_chave"
     try:
+        cached_data = buscar_dados(chave)
+        if cached_data:
+            return {"data": cached_data.decode("utf-8")}
         data = call_external_service(service_url)
+        redis_client.set(chave, data)
         return {"data": data}
-    except pybreaker.CircuitBreakerError:
-        raise HTTPException(status_code=503, detail="Serviço indisponível devido ao circuit breaker estar aberto")
 
+    except pybreaker.CircuitBreakerError:
+        # Se o circuit breaker estiver aberto, tentar obter os dados do Redis
+        cached_data = buscar_dados(chave)
+        if cached_data or None:
+            return {"data": cached_data.decode("utf-8")}
+        else:
+            raise HTTPException(status_code=503, detail="Serviço indisponível e não há dados em cache")
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
-
 
 if __name__ == "__main__":
     import uvicorn
